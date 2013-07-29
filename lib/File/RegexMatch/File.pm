3fba0e37dde66c26b4cb0d78246423bd871f2a02
Lloyd Griffiths <lloyd.griffiths.sw@gmail.com>
2013-07-29 12:31:20 +0100

    Added indentation to synopsis

--- lib/File/RegexMatch/File.pm
+++ lib/File/RegexMatch/File.pm
@@ -7,10 +7,10 @@ use File::stat;
 
 sub new {
     my ($class, %params) = @_;
-    my ($self) = { 
+    my ($self) = {
         path => undef,
     };
-    
+
     while (my ($key, $value) = each %params) {
         $self->{$key} = $value if exists $self->{$key};
     }
@@ -83,31 +83,28 @@ __END__
 
 File::RegexMatch::File - Object to represent a file.
 
-
 =head1 SYNOPSIS
 
-#!/usr/bin/env perl -w
+    #!/usr/bin/env perl -w
 
-use strict;
-use File::RegexMatch::File;
+    use strict;
+    use File::RegexMatch::File;
 
-File::RegexMatch::File->new(path => '/path/to/file');
-
+    File::RegexMatch::File->new(path => '/path/to/file');
 
 =head1 DESCRIPTION
 
 This module allows each file that has been located by File::RegexMatch
 to be represented by an object. The object has methods for retrieving
-information about the file using the File::stat module. There are 
-different levels of support for certain filesystems. See 
+information about the file using the File::stat module. There are
+different levels of support for certain filesystems. See
 L<stat|http://perldoc.perl.org/functions/stat.html> for further information.
-
 
 =head1 METHODS
 
 =head3 new
 
-Constructor for the object. Takes one parameter, which is the path of 
+Constructor for the object. Takes one parameter, which is the path of
 the file. The library will croak if stat cannot be called on the file
 path.
 
@@ -115,72 +112,58 @@ path.
 
 Returns the absolute path of the file.
 
-
 =head3 dev
 
 Returns the device number of the filesystem.
-
 
 =head3 ino
 
 Returns the inode number.
 
-
 =head3 mode
 
 Returns the type and permissions of the file.
-
 
 =head3 nlink
 
 Returns the number of hard links to the file.
 
-
 =head3 uid
 
 Returns the numeric user ID of the file owner.
-
 
 =head3 gid
 
 Returns the numeric group ID of the file owner.
 
-
 =head3 rdev
 
 Returns the device identifer.
-
 
 =head3 size
 
 Returns the total size of the file in bytes.
 
-
 =head3 atime
 
 Returns the last access time in seconds since the epoch.
-
 
 =head3 mtime
 
 Returns the last modify time in seconds since the epoch.
 
-
 =head3 ctime
 
 Returns the incode change time in seconds since the epoch.
-
 
 =head3 blksize
 
 Returns the preferred I/O size in bytes for interacting with the file.
 
-
 =head3 blocks
 
-Returns the actual number of system-specific blocks allocated on the 
+Returns the actual number of system-specific blocks allocated on the
 disk.
-
 
 =head1 SEE ALSO
 
@@ -189,22 +172,19 @@ File::stat
 L<stat|http://perldoc.perl.org/functions/stat.html>
 
 
-
 =head1 BUGS
 
 Please report any bugs or feature requests to lloydg@cpan.org
-
 
 =head1 AUTHOR
 
 Lloyd Griffiths
 
-
 =head1 COPYRIGHT
 
 Copyright (c) 2013 Lloyd Griffiths
 
-This program is free software; you can redistribute
+This library is free software; you can redistribute
 it and/or modify it under the same terms as Perl itself.
 
 =cut
